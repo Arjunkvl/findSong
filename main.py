7118341fb7044086326ea46abec1d75f9f5045bc
Arjunkvl <arjunaskvl@gmail.com>
2025-01-30 21:14:05 +0530

    commit

--- main.py
+++ main.py
@@ -2,20 +2,15 @@ import pyaudio
 import wave
 from shazamio import Shazam
 import asyncio
-
 format = pyaudio.paInt16
 channels = 1
 rate = 44100
 chunk = 1024
 record_sec = 10
 output_file = 'output.wav'
-
 p = pyaudio.PyAudio()
-
 stream = p.open(channels=channels,rate=rate,input=True,format=format,frames_per_buffer=chunk)
-
 print('Recording audio(10s)....')
-
 frames=[]
 for i in range(0,int(rate/chunk * record_sec)):
     data = stream.read(chunk)
@@ -24,16 +19,19 @@ print('Record Completed....')
 stream.stop_stream()
 stream.close()
 p.terminate()
-
 with wave.open(output_file,'wb') as wf:
     wf.setnchannels(channels)
     wf.setframerate(rate)
     wf.setsampwidth(p.get_sample_size(format))
     wf.writeframes(b''.join(frames))
-
 async def identify_song(audio_file):
     shazam = Shazam()
     out = await shazam.recognize(audio_file)
-    print(out)
-
+    song = out.get('track',{})
+    if song:
+        print(song['title'])
+    else:
+        print("Sorry cant find the song")
+        
+print('Analysing Audio....')
 asyncio.run(identify_song(output_file))
